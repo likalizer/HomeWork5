1a14d85dfa6dee115b205041403cd49cd7ecb170
likalizer <lika.lika1823@gmail.com>
2024-03-11 20:33:27 +0200

    Додані елементи інтерфейсу

---
+++ HomeWork5/HomeWork5/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  HomeWork5
+//
+//  Created by Лика Котик on 11.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var logIn: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+
+
+}
+
